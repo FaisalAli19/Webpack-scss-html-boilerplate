df8534f684390baf6ff7eaa6f7d935a26e111da1
FaisalAli <faisalali@FaisalAlis-MacBook-Pro.local>
2021-01-17 22:13:30 +0530

    created boilerplate for html scss and js project

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import "../css/styles.css";
+import "../scss/styles.scss";
+console.log("hello world!");
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+// eslint-disable-next-line no-unused-vars
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "index.html" }],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    open: true,
+  },
+};
+
+module.exports = config;
